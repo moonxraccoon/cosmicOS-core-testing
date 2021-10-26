59e859d6d54826438c5fed723460a6aa07de35f5
cosmicraccoon <sl7@flate.io>
2021-10-26 14:40:47 +0200

    fixed wrong delay configuration

--- src/stm32/f4/delay/delay.c
+++ src/stm32/f4/delay/delay.c
@@ -19,7 +19,7 @@ void delayMs(uint32_t ms) {
 
 
 void delayUs(uint32_t us) {
-    SysTick->LOAD = (apb1_freq/1000)/1000;
+    SysTick->LOAD = (ahb_freq/1000)/1000;
     SysTick->VAL = 0x00;
 
     SysTick->CTRL = CTRL_ENABLE | CTRL_CLKSRC;
